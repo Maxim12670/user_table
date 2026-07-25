ab21630458825d6f9d693f04bd60e7c36cf89083
akaikinmax <akaikinmax@mail.ru>
2026-07-25 16:37:36 +0700

    added base teamplate table

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <span>hello</span>
-    </>
-  );
-}
-
-export default App;
---
+++ src/app/App.jsx
@@ -0,0 +1,9 @@
+import Table from "./Table";
+
+export default function App() {
+  return (
+    <div style={{ maxWidth: "1400px", margin: "0 auto" }}>
+      <Table />
+    </div>
+  );
+}
